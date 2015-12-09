6f6606297eb495bedc985e5e839178e97dbf386e
gio.liao <gio.liao@ubitus.net>
2015-12-09 18:38:43 +0800

    1st
    Signed-off-by: gio.liao <gio.liao@ubitus.net>

---
+++ js/main.js
@@ -0,0 +1,72 @@
+var app = angular.module('ubitusWebApp', [
+  'ngRoute'
+]);
+
+
+//route
+
+app.config(['$routeProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+  // Home
+    .when("/", {
+      templateUrl: "pages/home.html",
+      controller: "PageCtrl"
+    })
+    // Pages
+    .when("/solutions", {
+      templateUrl: "pages/solutions.html",
+      controller: "PageCtrl"
+    })
+    .when("/products", {
+      templateUrl: "pages/faq.html",
+      controller: "PageCtrl",
+    })
+    .when("/credentials", {
+      templateUrl: "pages/pricing.html",
+      controller: "PageCtrl"
+    })
+    .when("/press", {
+      templateUrl: "pages/services.html",
+      controller: "PageCtrl"
+    })
+    .when("/aboutubitus", {
+      templateUrl: "pages/contact.html",
+      controller: "PageCtrl"
+    })
+    //press
+    .when("/press", {
+      templateUrl: "press/latest.html",
+      controller: "PageCtrl"
+    })
+    // else 404
+    .otherwise("/404", {
+      templateUrl: "pages/404.html",
+      controller: "PageCtrl"
+    });
+
+    //$locationProvider.html5Mode(true);
+}]);
+
+//controller
+
+app.controller('PageCtrl', function( /* $scope, $location, $http */ ) {
+
+
+
+  //slidshow
+  var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: 3000,
+    autoplayDisableOnInteraction: false,
+    loop: true
+  });
+
+  //get
+    $('.latest-press .date').load('../press/latest.html #press .title .date');
+    $('.latest-press h2').load('../press/latest.html #press .title h1');
+    $('.latest-press .summary').load('../press/latest.html #press .inner'); 
+});
+
